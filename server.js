744de3b0ea7eca605da01f60e3e2ea113c58a1f1
Andrey Petrov <rokanor@gmail.com>
2013-09-11 18:38:26 -0400

    fixing a bug

--- server.js
+++ server.js
@@ -31,4 +31,3 @@ if ('development' == app.get('env')) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-                             s
