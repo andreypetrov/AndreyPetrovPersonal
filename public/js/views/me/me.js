23037ffed6c870e8f73fba582ed3681d5d8a5f4e
Andrey Petrov <rokanor@gmail.com>
2013-09-16 02:15:52 -0400

    added classes for all nav views

---
+++ public/js/views/me/me.js
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrey
+ * Date: 2013-09-14
+ * Time: 5:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+define([
+    'jquery',
+    'views/archview',
+    'hbs!views/me/meTemplate'
+], function ($, ArchView, template) {
+    return ArchView.extend({
+        template: template
+    });
+});
